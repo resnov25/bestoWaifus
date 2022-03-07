0199d3552df2e3be3c96ec956ad1c6b16ef8fcbb
Andre Vignon <andre_viggar@hotmail.com>
2022-03-07 13:55:12 -0600

    Se agrega el service de Waifus Se modifica el controller para generar un modelo que instancie el service Se modifica el html con el uso de Thymeleafe "th" para la generacion de contendio dinamico

--- src/main/java/com/waifus/bestowaifus/controller/ListadoControler.java
+++ src/main/java/com/waifus/bestowaifus/controller/ListadoControler.java
@@ -1,15 +1,24 @@
 package com.waifus.bestowaifus.controller;
 
+import com.waifus.bestowaifus.domain.Waifu;
+import com.waifus.bestowaifus.service.WaifuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+import java.util.List;
 
+@Controller
 public class ListadoControler {
+   @Autowired
+   private WaifuService waifuService;
 
    @RequestMapping()
-   public String listarWaifus(){
-
+   public String listarWaifus(Model model){
+      List<Waifu> nuevaWaifu = waifuService.buscarWaifus();
+      model.addAttribute("Waifus", nuevaWaifu);
+      nuevaWaifu.forEach(System.out::println);
       return "listado";
    }
 
