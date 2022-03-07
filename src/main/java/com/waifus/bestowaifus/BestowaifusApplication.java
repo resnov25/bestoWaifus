40477c29a5720097e4b4870e97b3eb1a68e23435
Andre Vignon <andre_viggar@hotmail.com>
2022-03-07 11:28:21 -0600

    Inicio

---
+++ src/main/java/com/waifus/bestowaifus/BestowaifusApplication.java
@@ -0,0 +1,13 @@
+package com.waifus.bestowaifus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestowaifusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestowaifusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waifus/bestowaifus/BestowaifusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waifus.bestowaifus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestowaifusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
